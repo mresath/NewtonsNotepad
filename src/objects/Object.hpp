5bcf4422b05ef38b17273479cc24b47edc7e1e5d
justacasualign <justacasualign@gmail.com>
2025-10-14 20:30:22 +0300

    math fix for windows

--- src/objects/Object.hpp
+++ src/objects/Object.hpp
@@ -1,46 +1,58 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+
 #include <SFML/Graphics.hpp>
 #include <math.h>
 #include "objects/Body.hpp"
 #include "math/Util.hpp"
 
-enum ShapeType {
+enum ShapeType
+{
     CIRCLE,
     SQUARE
 };
 
-struct Object {
-    Body* body;
-    sf::Shape* shape;
+struct Object
+{
+    Body *body;
+    sf::Shape *shape;
 
     float r;
 
-    Object(float r, Body* b, sf::Shape* s) : body(b), shape(s), r(r) {}
-    Object(Vec2 position, float r, float mass, ShapeType type) {
+    Object(float r, Body *b, sf::Shape *s) : body(b), shape(s), r(r) {}
+    Object(Vec2 position, float r, float mass, ShapeType type)
+    {
         this->r = r;
         float len = metersToPixels(r);
         body = new Body(position, mass);
-        if (type == CIRCLE) {
+        if (type == CIRCLE)
+        {
             shape = new sf::CircleShape(len);
             shape->setOrigin(len, len);
             body->area = M_PI * r * r;
-        } else if (type == SQUARE) {
+        }
+        else if (type == SQUARE)
+        {
             shape = new sf::RectangleShape(sf::Vector2f(len, len));
             shape->setOrigin(len / 2, len / 2);
             body->area = r * r;
         }
         shape->setFillColor(sf::Color::White);
     }
-    Object(Vec2 position, float r, float mass, ShapeType type, float dragC) {
+    Object(Vec2 position, float r, float mass, ShapeType type, float dragC)
+    {
         this->r = r;
         body = new Body(position, mass);
         body->dragCoefficient = dragC;
-        if (type == CIRCLE) {
+        if (type == CIRCLE)
+        {
             shape = new sf::CircleShape(r);
             shape->setOrigin(r, r);
             body->area = M_PI * r * r;
-        } else if (type == SQUARE) {
+        }
+        else if (type == SQUARE)
+        {
             shape = new sf::RectangleShape(sf::Vector2f(r, r));
             shape->setOrigin(r / 2, r / 2);
             body->area = r * r;
@@ -48,15 +60,17 @@ struct Object {
         shape->setFillColor(sf::Color::White);
     }
 
-    void update(float dt) {
+    void update(float dt)
+    {
         body->update(dt);
 
-        Vec2 * pos = metersToPixels(&body->position);
+        Vec2 *pos = metersToPixels(&body->position);
         shape->setPosition(pos->x, pos->y);
         delete pos;
     }
 
-    void draw(sf::RenderWindow *window) {
+    void draw(sf::RenderWindow *window)
+    {
         window->draw(*shape);
     }
 };
